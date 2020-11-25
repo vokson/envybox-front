927121a598ffa971f35151b3fabbc7e87e4be044
Noskov Alexey <vokson@yandex.ru>
2020-11-25 20:40:35 +0300

    Ready for review

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,30 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import BootstrapVue from 'bootstrap-vue'
 import Home from '../views/Home.vue'
+import axios from 'axios'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 
 Vue.use(VueRouter)
+Vue.use(BootstrapVue)
 
 const routes = [
   {
     path: '/',
     name: 'Home',
     component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   }
 ]
 
 const router = new VueRouter({
   routes
 })
+
+window.$axios = axios.create({
+  baseURL: "http://envybox.backend/api"
+});
 
 export default router
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,143 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="page">
+    <div>
+      <b-form @submit="onSubmit" @reset="onReset">
+        <b-form-group
+          id="input-group-name"
+          label="Введите имя:"
+          label-for="input-name"
+        >
+          <b-form-input
+            id="input-name"
+            v-model="form.name"
+            placeholder="Ваше имя"
+          ></b-form-input>
+        </b-form-group>
+
+        <div v-if="errors">
+            <div v-for="(item, index) in errors.name" v-bind:key="index" class="alert alert-danger">{{ item }}</div>
+        </div>
+
+        <b-form-group
+          id="input-group-phone"
+          label="Введите номер телефона:"
+          label-for="input-phone"
+        >
+          <b-form-input
+            id="input-phone"
+            v-model="form.phone"
+            placeholder="10-ти значный номер без пробелов"
+          ></b-form-input>
+        </b-form-group>
+
+        <div v-if="errors">
+            <div v-for="(item, index) in errors.phone" v-bind:key="index" class="alert alert-danger">{{ item }}</div>
+        </div>
+
+        <b-form-group
+          id="input-group-text"
+          label="Обращение:"
+          label-for="input-text"
+        >
+          <b-form-textarea
+            id="input-text"
+            v-model="form.text"
+            placeholder="Введите что-нибудь..."
+            rows="3"
+            max-rows="6"
+          ></b-form-textarea>
+        </b-form-group>
+
+        <div v-if="errors">
+            <div v-for="(item, index) in errors.text" v-bind:key="index" class="alert alert-danger">{{ item }}</div>
+        </div>
+
+        <b-button type="submit" variant="primary" class="button"
+          >Отправить</b-button
+        >
+        <b-button type="reset" variant="danger" class="button"
+          >Сбросить</b-button
+        >
+      </b-form>
+    </div>
+
+    <div v-if="!on_progress" class="status_bar">
+      <div v-if="errors">
+          <div class="alert alert-danger">Ошибка выполнения запроса</div>
+      </div>
+
+      <div v-else>
+        <div class="alert alert-success">Данные успешно отправлены.</div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+  data() {
+    return {
+      url: "/feedback",
+      form: {
+        name: "",
+        phone: "",
+        text: "",
+      },
+      errors: null,
+      on_progress: false,
+      // status: null,
+    };
+  },
+  methods: {
+    onSubmit(evt) {
+      this.on_progress = true;
+
+      evt.preventDefault();
+
+      let successFunction = function () {
+        this.errors = null
+      };
+
+      let errorFunction = function (error) {
+        if (error.response) {
+          this.errors = error.response.data.errors;
+        }
+      };
+
+      let finalFunction = function () {
+        this.on_progress = false;
+      };
+
+      successFunction = successFunction.bind(this);
+      errorFunction = errorFunction.bind(this);
+      finalFunction = finalFunction.bind(this);
+
+      window.$axios
+        .post(this.url, this.form)
+        .then(successFunction)
+        .catch(errorFunction)
+        .then(finalFunction);
+    },
+
+    onReset(evt) {
+      evt.preventDefault();
+      this.form.text = "";
+      this.form.name = "";
+      this.form.phone = "";
+    },
+  },
+};
 </script>
+
+<style>
+.page {
+  margin: 1em;
+}
+.button {
+  margin-left: 1em;
+}
+.status_bar {
+  margin: 1em;
+}
+</style>
